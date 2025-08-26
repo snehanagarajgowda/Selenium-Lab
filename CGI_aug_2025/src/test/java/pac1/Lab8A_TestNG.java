fa0db2beb7f2c1a880fdcb655636c4e5d452bde2
Sneha <snehanagarajsnehanagaraj703@gmail.com>
2025-08-26 18:54:35 +0530

    This is lab 8 and 9

---
+++ CGI_aug_2025/src/test/java/pac1/Lab8A_TestNG.java
@@ -0,0 +1,99 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+import com.beust.jcommander.Parameters;
+ 
+import io.github.bonigarcia.wdm.WebDriverManager;
+ 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.BeforeClass;
+ 
+import java.time.Duration;
+ 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+ 
+public class Lab8A_TestNG {
+	WebDriver driver;
+  @Test(dataProvider = "dp")
+  public void f(Integer n, String s) {
+		//driver.get("https://tutorialsninja.com/demo/index.php?");
+	  
+	  driver.get("https://tutorialsninja.com/demo/index.php?");
+		driver.findElement(By.linkText("Desktops")).click();
+		driver.findElement(By.linkText("Mac (1)")).click();
+		WebElement sort = driver.findElement(By.id("input-sort"));
+		sort.sendKeys("Name(A-Z)");
+		driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div/div/div[2]/div[2]/button[1]")).click();
+		
+		
+		
+  }
+  
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("This is Before Method");
+	  WebDriverManager.chromedriver().setup();
+		 driver=new ChromeDriver();
+		 driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+  }
+ 
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("This is After Method");
+	  driver.quit();
+  }
+ 
+ 
+  @DataProvider
+  public Object[][] dp() {
+    return new Object[][] {
+      new Object[] { 1, "a" },
+      new Object[] { 2, "b" },
+    };
+  }
+  @BeforeClass
+  public void beforeClass() {
+	  System.out.println("This is Before Class");
+  }
+ 
+  @AfterClass
+  public void afterClass() {
+	  System.out.println("This is After class");
+  }
+ 
+  @BeforeTest
+  public void beforeTest() {
+	  System.out.println("This is Before Test");
+  }
+ 
+  @AfterTest
+  public void afterTest() {
+	  System.out.println("This is After Test");
+  }
+ 
+  @BeforeSuite
+  public void beforeSuite() {
+	  System.out.println("This is Before Suite");
+  }
+ 
+  @AfterSuite
+  public void afterSuite() {
+	  System.out.println("This is After Suite");
+  }
+ 
+}
+ 
